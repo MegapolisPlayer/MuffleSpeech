803609f27d60827c26dc2b180077e8020a0a3814
Martin <84806126+MegapolisPlayer@users.noreply.github.com>
2022-10-13 20:28:03 +0200

    Delete src directory

--- src/core/about.md
+++
@@ -1,2 +0,0 @@
-# About this component
-This is the core of MuffleSpeech as a dynamic library.
--- src/core/fileio/FileIO.h
+++
@@ -1,7 +0,0 @@
-#ifndef MUFFLESPEECH_FILE_IO
-#define MUFFLESPEECH_FILE_IO
-
-#include "Stats.h"
-#include "FileMap.h"
-
-#endif
